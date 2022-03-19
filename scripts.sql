1d8e89074ecccaca915375f69ea8aaeb99c85b96
ADMIN <jarksctc@gmail.com>
2022-03-19 13:32:01 +0530

    updated

--- scripts.sql
+++ scripts.sql
@@ -1,430 +1,116 @@
-USE [IRMS]
+USE [master]
 GO
-/****** Object:  Table [dbo].[Address]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+/****** Object:  Database [IRMS]    Script Date: 3/19/2022 11:29:42 AM ******/
+CREATE DATABASE [IRMS]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'IRMS', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\IRMS.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'IRMS_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\IRMS_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
-SET QUOTED_IDENTIFIER ON
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [IRMS].[dbo].[sp_fulltext_database] @action = 'enable'
+end
 GO
-CREATE TABLE [dbo].[Address](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[AddressLine1] [varchar](200) NULL,
-	[AddressLine2] [varchar](200) NULL,
-	[Country] [varchar](50) NULL,
-	[State] [varchar](50) NULL,
-	[City] [varchar](50) NULL,
-	[PostalCode] [char](8) NULL,
-	[AddressType] [bit] NULL,
-	[EmployeeId] [int] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET ANSI_NULL_DEFAULT OFF 
 GO
-/****** Object:  Table [dbo].[Appointment]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET ANSI_NULLS OFF 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET ANSI_PADDING OFF 
 GO
-CREATE TABLE [dbo].[Appointment](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeId] [int] NULL,
-	[CompanyId] [int] NULL,
-	[RefNumberType] [varchar](20) NULL,
-	[DiscussionHeldOn] [datetimeoffset](7) NULL,
-	[DateOfJoining] [datetimeoffset](7) NULL,
-	[CTC] [decimal](18, 0) NULL,
-	[CTCinWords] [varchar](100) NULL,
-	[ReportingType] [int] NULL,
-	[ReportingTime] [datetimeoffset](7) NULL,
-	[ReportingMailId] [varchar](50) NULL,
-	[ModeOfPaymentType] [int] NULL,
-	[NetSalary] [decimal](18, 0) NULL,
-	[Deductions] [decimal](18, 0) NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetimeoffset](7) NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetimeoffset](7) NULL,
- CONSTRAINT [PK_dbo.JARKS_TBL_Appointment] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET ANSI_WARNINGS OFF 
 GO
-/****** Object:  Table [dbo].[Attendence]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET ARITHABORT OFF 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET AUTO_CLOSE OFF 
 GO
-CREATE TABLE [dbo].[Attendence](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeId] [int] NULL,
-	[LeaveDate] [datetime] NULL,
-	[LeaveType] [int] NULL,
-	[Reason] [varchar](500) NULL,
-	[CreatedBy] [int] NULL,
-	[CreatedDate] [datetimeoffset](7) NULL,
-	[ModifiedBy] [int] NULL,
-	[ModifiedDate] [datetimeoffset](7) NULL,
- CONSTRAINT [PK_JARKS_TBL_Attendence] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET AUTO_SHRINK OFF 
 GO
-/****** Object:  Table [dbo].[Company]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET AUTO_UPDATE_STATISTICS ON 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
-CREATE TABLE [dbo].[Company](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [nvarchar](50) NULL,
-	[FileId] [nvarchar](50) NULL,
-	[Group] [nvarchar](50) NULL,
-	[Mobile] [nvarchar](20) NULL,
-	[Landline] [nvarchar](20) NULL,
-	[CreatedBy] [int] NULL,
-	[CreatedDate] [datetimeoffset](7) NULL,
-	[ModifiedBy] [int] NULL,
-	[ModifiedDate] [datetimeoffset](7) NULL,
- CONSTRAINT [PK_Company] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET CURSOR_DEFAULT  GLOBAL 
 GO
-/****** Object:  Table [dbo].[Education]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET NUMERIC_ROUNDABORT OFF 
 GO
-CREATE TABLE [dbo].[Education](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[InstituteName] [varchar](50) NULL,
-	[School_College] [varchar](50) NULL,
-	[Class_Degree] [varchar](50) NULL,
-	[Place] [varchar](50) NULL,
-	[Securing] [decimal](2, 2) NULL,
-	[PassOut] [int] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_Education] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET QUOTED_IDENTIFIER OFF 
 GO
-/****** Object:  Table [dbo].[EmailTemplates]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET RECURSIVE_TRIGGERS OFF 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET  DISABLE_BROKER 
 GO
-CREATE TABLE [dbo].[EmailTemplates](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[EmailType] [int] NULL,
-	[Template] [varchar](max) NULL,
-	[PlaceHolders] [varchar](500) NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_EmailTemplates] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
-/****** Object:  Table [dbo].[Employee]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET TRUSTWORTHY OFF 
 GO
-CREATE TABLE [dbo].[Employee](
-	[Id] [int] IDENTITY(500,1) NOT NULL,
-	[FirstName] [varchar](50) NULL,
-	[LastName] [varchar](50) NULL,
-	[MiddleName] [varchar](50) NULL,
-	[DesignationType] [varchar](50) NULL,
-	[DepartmentType] [varchar](50) NULL,
-	[GenderType] [int] NULL,
-	[DOB] [varchar](100) NULL,
-	[MaritalStatus] [varchar](50) NULL,
-	[Mobile] [varchar](20) NULL,
-	[Home] [varchar](20) NULL,
-	[EmailId] [varchar](50) NULL,
-	[AltEmailId] [varchar](50) NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_tbl_EMPINFO] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
-/****** Object:  Table [dbo].[Expenses]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET PARAMETERIZATION SIMPLE 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
-CREATE TABLE [dbo].[Expenses](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[ExpenseType] [varchar](50) NULL,
-	[Name] [varchar](50) NULL,
-	[Quantity] [decimal](2, 2) NULL,
-	[Price] [decimal](2, 2) NULL,
-	[TotalPrice] [decimal](2, 2) NULL,
-	[ItemDate] [datetime] NULL,
-	[EnteredBy] [varchar](50) NULL,
-	[ToWhom] [varchar](50) NULL,
-	[Status] [bit] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_Expenses] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET HONOR_BROKER_PRIORITY OFF 
 GO
-/****** Object:  Table [dbo].[Experience]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET RECOVERY FULL 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET  MULTI_USER 
 GO
-CREATE TABLE [dbo].[Experience](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[Organization] [varchar](50) NULL,
-	[TechnologiesWorkedOn] [varchar](50) NULL,
-	[Position] [varchar](50) NULL,
-	[DateOfJoin] [datetime] NULL,
-	[DateOfRelieving] [datetime] NULL,
-	[LastSalary] [varchar](50) NULL,
-	[Reason] [varchar](50) NULL,
-	[CTC] [money] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_Experience] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET PAGE_VERIFY CHECKSUM  
 GO
-/****** Object:  Table [dbo].[FamilyDetails]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET DB_CHAINING OFF 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
-CREATE TABLE [dbo].[FamilyDetails](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[Relationship] [varchar](50) NULL,
-	[PersonName] [varchar](50) NULL,
-	[Occupation] [varchar](50) NULL,
-	[Age] [int] NULL,
-	[Income] [money] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_Emp_FamilyDetails] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
-/****** Object:  Table [dbo].[File]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET DELAYED_DURABILITY = DISABLED 
 GO
-SET QUOTED_IDENTIFIER ON
+
+ALTER DATABASE [IRMS] SET ACCELERATED_DATABASE_RECOVERY = OFF  
 GO
-CREATE TABLE [dbo].[File](
-	[myphoto] [image] NULL,
-	[name] [varchar](50) NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER DATABASE [IRMS] SET QUERY_STORE = OFF
 GO
-/****** Object:  Table [dbo].[Income]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
+
+ALTER DATABASE [IRMS] SET  READ_WRITE 
 GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Income](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[IncomeType] [varchar](50) NULL,
-	[Name] [varchar](50) NULL,
-	[Quantity] [decimal](2, 2) NULL,
-	[Price] [decimal](2, 2) NULL,
-	[TotalPrice] [decimal](2, 2) NULL,
-	[ToWhom] [varchar](50) NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_Income] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Menu]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Menu](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[Name] [varchar](100) NULL,
-	[Description] [varchar](max) NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_SNGSMenu] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Offer]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Offer](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[RefNo] [int] NULL,
-	[DOJ] [datetime] NULL,
-	[Desgination] [varchar](50) NULL,
-	[CTC] [money] NULL,
-	[ReportingTo] [varchar](50) NULL,
-	[Department] [varchar](50) NULL,
-	[ReportingDate] [datetime] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[PaySlip]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[PaySlip](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Employee_Name] [varchar](50) NULL,
-	[Employee_Code] [varchar](50) NULL,
-	[PaySlip_Month] [varchar](50) NULL,
-	[PaySlip_Year] [varchar](50) NULL,
-	[Employee_Bank_Name] [varchar](50) NULL,
-	[Employee_bank_Account_No] [varchar](50) NULL,
-	[Employee_Gross_Salary] [varchar](50) NULL,
-	[Employee_Net_Salary] [varchar](50) NULL,
-	[Employee_Deductions] [varchar](50) NULL,
-	[Employee_AmountInWords] [varchar](50) NULL,
-	[Employee_Lop_Days] [varchar](50) NULL,
-	[EmployeeID] [int] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_PaySlip] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Queries]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Queries](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](50) NULL,
-	[QueryType] [varchar](50) NULL,
-	[Description] [varchar](500) NULL,
-	[EmailID] [varchar](50) NULL,
-	[Mobile] [int] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_Feedback] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Releaving]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Releaving](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[RefNo] [varchar](50) NULL,
-	[EmpName] [varchar](50) NULL,
-	[Desgination] [varchar](50) NULL,
-	[DOJ] [datetime] NULL,
-	[ResignationApplied] [datetime] NULL,
-	[DateOfReliving] [datetime] NULL,
-	[EmployeeID] [int] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_SNGS_Emp_Reliving] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Salary]    Script Date: 3/19/2022 11:53:26 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Salary](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[EmployeeID] [int] NULL,
-	[Salary] [money] NULL,
-	[SalaryDate] [datetime] NULL,
-	[CreatedBy] [varchar](50) NULL,
-	[CreatedDate] [datetime] NULL,
-	[ModifiedBy] [varchar](50) NULL,
-	[ModifiedDate] [datetime] NULL,
- CONSTRAINT [PK_JARKS_TBL_Salary] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Address]  WITH CHECK ADD  CONSTRAINT [FK_Address_Employee] FOREIGN KEY([EmployeeId])
-REFERENCES [dbo].[Employee] ([Id])
-GO
-ALTER TABLE [dbo].[Address] CHECK CONSTRAINT [FK_Address_Employee]
-GO
-ALTER TABLE [dbo].[Appointment]  WITH CHECK ADD  CONSTRAINT [FK_Appointment_Employee] FOREIGN KEY([EmployeeId])
-REFERENCES [dbo].[Employee] ([Id])
-GO
-ALTER TABLE [dbo].[Appointment] CHECK CONSTRAINT [FK_Appointment_Employee]
-GO
+
+
